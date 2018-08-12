984b7bc876c8e3061c0a03a9dd0e216e1920a3ba
Renê Xavier <renefx@gmail.com>
2018-08-12 00:33:18 -0300

    Aula 05 - Autolayout

---
+++ iOS 1/Aula 5/Projeto Constraints/Projeto Lojas/Projeto Lojas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projeto Lojas
+//
+//  Created by Renê Xavier on 28/06/18.
+//  Copyright © 2018 com.iesb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
